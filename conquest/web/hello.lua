c7ce9f2baefb2f8a9295ac3e34785397065881b1
toskrip <tomas@skripcak.net>
2023-11-16 09:45:01 +0100

    feat: DICOM echo self check in hello service

--- conquest/web/hello.lua
+++ conquest/web/hello.lua
@@ -1,5 +1,11 @@
 -- This script is querying ConQuest PACS in order to dummy test PACS API
 
+-- Helper functions declaration
+
+function isempty(s)
+    return s == nil or s == '';
+end
+
 -- RESPONSE
 
 print('Content-type: text/plain\n');
@@ -10,4 +16,17 @@ else
     s = source;
 end
 
-print(s .. [[ says: pong]]);
+if not isempty(s) then
+    response = servercommand('echo:'..s);
+end
+
+if not isempty(s) then
+    template = s..' is UP';
+    if response == template then
+        print(s .. [[ says: pong]]);
+    else
+        print(s .. [[ says: error]]);
+    end
+else
+    print('PACS says: error');
+end
